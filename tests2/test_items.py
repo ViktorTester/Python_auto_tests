bd89f2ff243ad9e5be59e1e6f89a74e8f6cd3ec8
VIktor <viktor345@inbox.lv>
2022-07-29 16:15:35 +0300

    test_items.py fix

--- tests2/test_items.py
+++ tests2/test_items.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_add_card_button(browser):
     browser.get(link)
